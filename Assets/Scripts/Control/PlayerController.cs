f1f8db886ccc0ae1e8e77eb4d8f3c6f0b0d8ccb1
Tomasz Leśniak <jamesshino152@gmail.com>
2020-04-24 18:01:29 +0200

    Refactored code and hierarchy

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform _target;
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+           MoveToCursor();
+        }
+
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
