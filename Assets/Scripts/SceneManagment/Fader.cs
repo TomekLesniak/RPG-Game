39bbdf247b91a368129dcf2beaed696303f83d7d
Tomasz Leśniak <jamesshino152@gmail.com>
2020-05-01 13:08:09 +0200

    Fade effect between scenes

---
+++ Assets/Scripts/SceneManagment/Fader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        [SerializeField] private float time = 1f;
+        
+        private CanvasGroup canvasGroup;
+        private float deltaAlpha;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            canvasGroup.alpha = 0;
+
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            yield return FadeIn(3f);
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            deltaAlpha = Time.deltaTime / time;
+            while (canvasGroup.alpha < 1f)
+            {
+                canvasGroup.alpha += deltaAlpha;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            deltaAlpha = Time.deltaTime / time;
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= deltaAlpha;
+                yield return null;
+            }
+        }
+    }
+
+}
